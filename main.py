dd6f5da299476f278fee01481d27397a2476bfeb
= <cavaleiromiau@gmail.com>
2025-10-14 21:39:05 -0300

    test

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ from tkinter import messagebox
 class SistemadeRegistro:
     def __init__(self):
         self.conn = sqlite3.connect('study.db')
+        self.c = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.c.execute('''CREATE TABLE IF NOT EXISTS students 
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       name TEXT NOT NULL, 
+                       Email TEXT NOT NULL,
+                       tel TEXT NOT NULL,
+                       sex TEXT NOT NULL,
+                       ''')
