5a715bfd5f72c451772ec20b7b7ff9fb180bdb1e
= <cavaleiromiau@gmail.com>
2025-10-15 20:21:22 -0300

    test

--- main.py
+++ main.py
@@ -14,4 +14,24 @@ class SistemadeRegistro:
                        Email TEXT NOT NULL,
                        tel TEXT NOT NULL,
                        sex TEXT NOT NULL,
-                       ''')
+                       date of birth Text NOT NULL,
+                       address TEXT NOT NULL,
+                       course text NOT NULL,
+                       picture TEXT NOT NULL)''')
+        
+def register_student(self, student):
+    self.c.execute("INSERT INTO students(name, email, tel, sex, date_of_birth, address, course, picture) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                   (student))
+    self.conn.commit()
+
+    #mostrando mensagem de sucesso
+    messagebox.showinfo("Sucess", "Student registered successfully!")
+
+    def view_all_students(self):
+        self.c.execute("SELECT * FROM students")
+        dados = self.fetchall()
+
+        for i in dados:
+            print(f'ID: {i[0]} | Name: {i[1]} | Email: {i[2]} | Tel: {i[3]} | Sex: {i[4]} | Date of Birth: {i[5]} | Address: {i[6]} | Course: {i[7]} | Picture: {i[8]}')
+        return dados
+        
