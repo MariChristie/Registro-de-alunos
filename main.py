c217251168f60553158e5a7db926699967960f71
= <cavaleiromiau@gmail.com>
2025-10-16 20:07:37 -0300

    test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class SistemadeRegistro:
         self.create_table()
 
     def create_table(self):
-        self.c.execute('''CREATE TABLE IF NOT EXISTS students 
+        self.c.execute('''CREATE TABLE IF NOT EXISTS students (
                        id INTEGER PRIMARY KEY AUTOINCREMENT,
                        name TEXT NOT NULL, 
                        Email TEXT NOT NULL,
@@ -29,9 +29,32 @@ def register_student(self, student):
 
     def view_all_students(self):
         self.c.execute("SELECT * FROM students")
-        dados = self.fetchall()
+        dados = self.c.fetchall()
 
         for i in dados:
             print(f'ID: {i[0]} | Name: {i[1]} | Email: {i[2]} | Tel: {i[3]} | Sex: {i[4]} | Date of Birth: {i[5]} | Address: {i[6]} | Course: {i[7]} | Picture: {i[8]}')
-        return dados
         
+    def search_student(self, id):
+        self.c.execute("SELECT * FROM students WHERE id=?", (id,))
+        dados = self.c.fetchall()
+
+        print(f'ID: {dados[0]} | Name: {dados[1]} | Email: {dados[2]} | Tel: {dados[3]} | Sex: {dados[4]} | Date of Birth: {dados[5]} | Address: {dados[6]} | Course: {dados[7]} | Picture: {dados[8]}')
+
+    def update_student(self, nova_valores):
+        query = "UPDATE students SET name=?, email=?, tel=?, sex-?, data_of_birth=?, address=?, course=?, picture=? WHERE id=?"
+        self.c.execute(query, nova_valores)
+        self.conn.commit()
+
+        #mostrando mensagem de sucesso
+        messagebox.showinfo("Sucess", f"Student with ID {nova_valores[8]} updated successfully!")
+    
+    def delete_student(self, id):
+        self.c.execute("DELETE FROM students WHERE id=?", (id,))
+        self.conn.commit()
+
+        #mostrando mensagem de sucesso
+        messagebox.showinfo("Sucess", f"Student with ID{id} deleted successfully!")
+
+
+# Criando uma instância do sistema de registro
+sistema_de_registro = SistemadeRegistro()
