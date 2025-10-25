5f885c4fecbe98eefe7b00f1227134751d71dd9f
= <cavaleiromiau@gmail.com>
2025-10-24 21:09:56 -0300

    test

---
+++ tela.py
@@ -0,0 +1,52 @@
+
+# importando dependencias do Tkinter
+from tkinter.ttk import *
+from tkinter import*
+from tkinter import ttk
+from tkinter import messagebox
+from tkinter import filedialog as fd
+
+# importando pillow
+from PIL import ImageTk, Image
+
+# tk calendar
+from tkcalendar import Calendar, DateEntry
+from datetime import date
+
+# cores
+co0 = "#2e2d2b"  # Preta
+co1 = "#feffff"  # Branca   
+co2 = "#e5e5e5"  # grey
+co3 = "#00a095"  # Verde
+co4 = "#403d3d"   # letra
+co6 = "#003452"   # azul
+co7 = "#ef5350"   # vermelha
+
+co6 = "#146C94"   # azul
+co8 = "#263238"   # + verde
+co9 = "#e9edf5"   # + verde
+
+#criando janela
+janela = Tk()
+janela.title("")
+janela.geometry("810x535")
+janela.configure(background=co1)
+janela.resizable(width=FALSE, height=FALSE)
+
+style = Style(janela)
+style.theme_use("clam")
+
+# Criando Frames
+frame_logo = Frame(janela, width=850, height=52, bg=co6)
+frame_logo.grid(row=0, column=0, pady=0, padx=0, sticky=NSEW, columnspan=5)
+
+frame_botoes = Frame(janela, width=100, height=200, bg=co2, relief=RAISED)
+frame_botoes.grid(row=1, column=0, pady=1, padx=0, sticky=NSEW)
+
+frame_details = Frame(janela, width=800, height=100, bg=co2, relief=SOLID)
+frame_details.grid(row=1, column=1, pady=1, padx=10, sticky=NSEW)
+
+frame_tabela = Frame(janela, width=800, height=100, bg=co2, relief=SOLID)
+frame_tabela.grid(row=3, column=0, pady=0, padx=0, sticky=NSEW, columnspan=5)
+
+janela.mainloop()
